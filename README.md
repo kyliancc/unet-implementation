c92b2714dd310ea96484dfe846c2fd23dc114ce3
Ruopu Chu <kylianchucs@outlook.com>
2024-06-06 16:13:10 +0800

    add U-Net implementation

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # unet-implementation
-My U-Net implementation
+My U-Net implementation using PyTorch.
+
+**This PyTorch implementation is preparation for my DDPM (Deep Diffusion Probablistic Model) implementation. Waiting for that!** 😁
+
+U-Net original research paper: https://arxiv.org/pdf/1505.04597
+
+> **WARN:**  
+This U-Net implementation is modified for the equal size between input and output. 
+So all non-padding 3x3 convolution layers was changed with padding=1.
+Besides, the copy and crop trick was changed to simply concatenation.
---
+++ unet.py
@@ -0,0 +1,71 @@
+import torch
+import torch.nn as nn
+
+
+class DoubleConv(nn.Module):
+    def __init__(self, in_channels, out_channels):
+        super().__init__()
+        self.model = nn.Sequential(
+            nn.Conv2d(in_channels, out_channels, 3, 1, 1, bias=False),
+            nn.BatchNorm2d(out_channels),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(out_channels, out_channels, 3, 1, 1, bias=False),
+            nn.BatchNorm2d(out_channels),
+            nn.ReLU(inplace=True)
+        )
+
+    def forward(self, x):
+        x = self.model(x)
+        return x
+
+
+class Down(nn.Module):
+    def __init__(self, in_channels, out_channels):
+        super().__init__()
+        self.down_conv = nn.Sequential(
+            nn.MaxPool2d(2),
+            DoubleConv(in_channels, out_channels)
+        )
+
+    def forward(self, x):
+        return self.down_conv(x)
+
+
+class Up(nn.Module):
+    def __init__(self, in_channels, out_channels):
+        super().__init__()
+        self.up = nn.ConvTranspose2d(in_channels, out_channels, 2, 2, 0)
+        self.conv = DoubleConv(in_channels, out_channels)
+
+    def forward(self, x, skip_x):
+        x = self.up(x)
+        x = torch.cat([skip_x, x], dim=1)
+        return self.conv(x)
+
+
+class UNet(nn.Module):
+    def __init__(self, in_channels, out_channels):
+        super().__init__()
+        self.in_conv = DoubleConv(in_channels, 64)
+        self.down1 = Down(64, 128)
+        self.down2 = Down(128, 256)
+        self.down3 = Down(256, 512)
+        self.down4 = Down(512, 1024)
+        self.up1 = Up(1024, 512)
+        self.up2 = Up(512, 256)
+        self.up3 = Up(256, 128)
+        self.up4 = Up(128, 64)
+        self.out_conv = nn.Conv2d(64, out_channels, 1, 1, 0)
+
+    def forward(self, x):
+        x1 = self.in_conv(x)
+        x2 = self.down1(x1)
+        x3 = self.down2(x2)
+        x4 = self.down3(x3)
+        x5 = self.down4(x4)
+        x = self.up1(x5, x4)
+        x = self.up2(x, x3)
+        x = self.up3(x, x2)
+        x = self.up4(x, x1)
+        x = self.out_conv(x)
+        return x
